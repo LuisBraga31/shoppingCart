db293334d34e26358912f80eb7ec96e2079bae6a
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-13 21:37:17 -0300

    Add a formatter

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card } from "react-bootstrap"
+import { formatter } from "../utilities/formatter"
 
 type StoreItemProps = {
    id: number,
@@ -15,7 +16,7 @@ export const StoreItem = ( {id, name, price, imgUrl } : StoreItemProps) => {
         <Card.Body className="d-flex flex-column">
             <Card.Title className="d-flex justify-content-between align-items-baseline mb-4"> 
                 <span className="fs-2"> {name} </span>
-                <span className="ms-2 text-muted"> {price} </span>
+                <span className="ms-2 text-muted"> {formatter(price)} </span>
             </Card.Title>
         </Card.Body>
     </Card>
