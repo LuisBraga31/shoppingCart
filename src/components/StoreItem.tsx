def4dd1d5915ea9859ba9c946b0d15517e453690
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-13 21:47:10 -0300

    Finishing the StoreItem card style

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 import { formatter } from "../utilities/formatter"
 
 type StoreItemProps = {
@@ -10,14 +10,30 @@ type StoreItemProps = {
 
 
 export const StoreItem = ( {id, name, price, imgUrl } : StoreItemProps) => {
-  return (
-    <Card>
+  
+    const quantity = 0;
+  
+    return (
+    <Card className="h-100">
         <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit: "cover"}} />
         <Card.Body className="d-flex flex-column">
             <Card.Title className="d-flex justify-content-between align-items-baseline mb-4"> 
                 <span className="fs-2"> {name} </span>
                 <span className="ms-2 text-muted"> {formatter(price)} </span>
             </Card.Title>
+            <div className="mt-auto">
+                {quantity === 0 ? 
+                <Button className="w-100"> + Add to Cart </Button> 
+                    : 
+                <div className="d-flex align-items-center flex-column" style={{gap: ".5rem"}}>
+                    <div className="d-flex align-items-center justify-content-center" style={{gap: ".5rem"}}>
+                        <Button> - </Button>
+                            <div> <span className="fs-3"> {quantity}  </span> in Cart </div>
+                        <Button> + </Button>
+                    </div>
+                    <Button variant="danger" size="sm"> Remove </Button>
+                </div> }
+            </div>
         </Card.Body>
     </Card>
   )
