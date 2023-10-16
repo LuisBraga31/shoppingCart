9b3c4e7a3fb21a0564d128c0bd73b3b7d0b1cc39
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-15 21:19:20 -0300

    Creating new functions to cart

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -5,6 +5,10 @@ type ShoppingCartProviderProps = {
 }
 
 type ShoppingCartContext = {
+    cartQuantity: number,
+    cartItems: CartItem[],
+    openCart: () => void,
+    closeCart: () => void,
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
     decreaseCartQuantity: (id: number) => void
@@ -23,7 +27,13 @@ export function useShoppingCart() {
 }
 
 export function ShoppingCartProvider( { children } : ShoppingCartProviderProps ) {
+    const [isOpen, setIsOpen] = useState(false);
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    const cartQuantity = cartItems.reduce((quantity, item) => item.quantity + quantity, 0 );
+
+    const openCart = () => setIsOpen(true);
+    const closeCart = () => setIsOpen(false);
 
     function getItemQuantity(id: number) {
         return cartItems.find(item => item.id === id)?.quantity || 0;
@@ -68,7 +78,7 @@ export function ShoppingCartProvider( { children } : ShoppingCartProviderProps )
     }
 
     return(
-        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart }}>
+        <ShoppingCartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartItems, cartQuantity, openCart, closeCart }}>
             {children}
         </ShoppingCartContext.Provider>
     )
