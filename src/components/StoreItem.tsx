49492ba23f7af09dc62cf0a58dd5abe04f3ec7d2
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2023-10-20 18:24:46 -0300

    Adding new products, change img css and change the currency formatter type

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -16,7 +16,7 @@ export const StoreItem = ( {id, name, price, imgUrl } : StoreItemProps) => {
   
     return (
     <Card className="h-100">
-        <Card.Img variant="top" src={imgUrl} height="200px" style={{objectFit: "cover"}} />
+        <Card.Img variant="top" src={imgUrl} height="250px" style={{objectFit: "contain"}} />
         <Card.Body className="d-flex flex-column">
             <Card.Title className="d-flex justify-content-between align-items-baseline mb-4"> 
                 <span className="fs-2"> {name} </span>
